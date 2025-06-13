d8962d321247d565c67d30bc40071ff644f2d1db
Jay <jp6631@srmist.edu.in>
2025-06-13 15:01:21 +0530

    Building chatbot for food delivery app

--- backend/main.py
+++ backend/main.py
@@ -17,20 +17,39 @@ async def handle_request(request: Request):
     output_contexts = payload['queryResult']['outputContexts']
     # session_id = generic_helper.extract_session_id(output_contexts[0]["name"])
 
-    if intent == "track-order - context:ongoing-tracking":
-        return track_order(parameters)
+    intent_handler_dict = {
+        'order-add - context:ongoing order': add_to_order,
+        # 'order-remove - context:ongoing order': remove_from_order,
+        # 'order-complete - context:ongoing-order': complete_order,
+        'track-order - context:ongoing-tracking': track_order
+    }
+    return intent_handler_dict[intent](parameters)
+
+def add_to_order(parameters: dict):
+    food_items = parameters["food-item-names"]
+    quantities = parameters["number"]
+
+    if len(food_items) != len(quantities):
+        fulfillment_text = "Sorry I didn't understand.Can you please specify the quantity."
+    else:
+        fulfillment_text = f"Recieved {food_items} and {quantities} "
 
     return JSONResponse(content={
-        "fulfillmentText": f"Intent '{intent}' not handled explicitly."
+        "fulfillmentText": fulfillment_text
     })
 
 
 def track_order(parameters: dict):
-    order_id = parameters.get('order_id')
+    order_id = int(parameters['order_id'])
     order_status = db_helper.get_order_status(order_id)
 
+    if order_status:
+        fulfillment_text = f"The order status for order id: {order_id} is: {order_status}"
+    else:
+        fulfillment_text = f"No order found with order id: {order_id}"
+
     return JSONResponse(content={
-        "fulfillmentText": f"Tracking order with ID: {order_id}"
+        "fulfillmentText": fulfillment_text
     })
 
 
