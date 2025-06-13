1ba47e83db7e4e3f3f74b710e1fe5e64d2cbacfb
Jay <jp6631@srmist.edu.in>
2025-06-13 12:17:28 +0530

    Building chatbot for food delivery app

--- backend/main.py
+++ backend/main.py
@@ -1,7 +1,33 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, Request
+from fastapi.responses import JSONResponse
 
 app = FastAPI()
 
-@app.get("/")
-async def root():
-    return {"message": "Hello World"}
+inprogress_orders = {}
+
+@app.post("/")
+async def handle_request(request: Request):
+    # Retrieve the JSON data from the request
+    payload = await request.json()
+
+    # Extract necessary information from the Dialogflow WebhookRequest
+    intent = payload['queryResult']['intent']['displayName']
+    parameters = payload['queryResult']['parameters']
+    output_contexts = payload['queryResult']['outputContexts']
+    # session_id = generic_helper.extract_session_id(output_contexts[0]["name"])
+
+    if intent == "track-order - context:ongoing-tracking":
+        return track_order(parameters)
+
+    return JSONResponse(content={
+        "fulfillmentText": f"Intent '{intent}' not handled explicitly."
+    })
+
+
+def track_order(parameters: dict):
+    order_id = parameters.get('order_id')
+    return JSONResponse(content={
+        "fulfillmentText": f"Tracking order with ID: {order_id}"
+    })
+
+
